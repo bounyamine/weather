e83f7370e588f35f54da6ccae277e3202fec5c66
bounyamine <ousmanoubounyamine@gmail.com>
2024-09-19 17:54:51 +0100

    Change Api

--- src/App.js
+++ src/App.js
@@ -1,44 +1,83 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
 function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const ville = "lubumbashi"; // Nom de la ville
-  const urlapi = `https://cweather.vercel.app/${ville}`;
+  const [city, setCity] = useState('Paris');
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(false);
 
-  useEffect(() => {
-    fetch(urlapi)
-      .then(response => response.json())
-      .then(data => {
-        setData(data);
-      })
-      .catch(error => {
-        console.error(error);
-        setError("Erreur lors de la récupération de la météo.");
-      });
-  }, [urlapi]);
+  const handleSearch = () => {
+    if (city === '') return;
 
-  if (error) {
-    return <p>{error}</p>;
-  }
+    const url = `https://cweather.vercel.app/${city}`;
 
-  if (!data) {
-    return <p>Chargement...</p>;
-  }
+    fetch(url)
+        .then(response => response.json())
+        .then((json) => {
+          if (json.cod === '404') {
+            setError(true);
+            setWeather(null);
+          } else {
+            setError(false);
+            setWeather(json);
+          }
+        })
+        .catch(() => {
+          setError(true);
+        });
+  };
 
   return (
-    <div className="contenair">
-      <h1 className="pays">{data.lieu.pays}</h1>
-      <h3 className="ville">• {data.lieu.nom}</h3>
-      <div className="img">
-        <img id="weatherImage" src={`https:${data.meteo.icone}`} alt="weather" />
+    <div className="container" style={{ height: weather || error ? '590px' : '105px' }}>
+      <div className="search-box">
+        <i className="fa-solid fa-location-dot"></i>
+        <input
+          type="text"
+          placeholder="Enter your location"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <button className="fa-solid fa-magnifying-glass" onClick={handleSearch}></button>
       </div>
-      <p className="temp">{`${data.actuel.temp_c} °C / ${data.actuel.temp_f} °F`}</p>
-      <p className="meteo">{data.meteo.principal}</p>
-      <div className="lat-lon">
-        <p className="lat">Latitude : {data.lat}°</p>
-        <p className="lon">Longitude : {data.lon}°</p>
-      </div>
+
+      {error && (
+        <div className="not-found fadeIn">
+          <img src="images/404.png" alt="Not Found" />
+          <p>Oops! Invalid location :/</p>
+        </div>
+      )}
+
+      {weather && (
+        <div className="weather-box fadeIn">
+          <img
+            src={`https:${weather.meteo.icone}`}
+            alt={weather.meteo.principal}
+          />
+          <p className="temperature">{parseInt(weather.actuel.temp_c)}<span>°C</span></p>
+          <p className="description">{weather.meteo.principal}</p>
+        </div>
+      )}
+
+      {weather && (
+        <div className="weather-details fadeIn">
+          <div className="humidity">
+            <i className="fa-solid fa-water"></i>
+            <div className="text">
+              <span>{weather.actuel.humidite}%</span>
+              <p>Humidity</p>
+            </div>
+          </div>
+          <div className="wind">
+            <i className="fa-solid fa-wind"></i>
+            <div className="text">
+              <span>{parseInt(weather.actuel.vitesse_vent_kph)} Km/h</span>
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
