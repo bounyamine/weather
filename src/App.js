5dd96cc33660c1f05da36eecb67145ae8020a3bc
bounyamine <ousmanoubounyamine@gmail.com>
2024-09-19 16:52:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const ville = "lubumbashi"; // Nom de la ville
+  const urlapi = `https://cweather.vercel.app/${ville}`;
+
+  useEffect(() => {
+    fetch(urlapi)
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
+      })
+      .catch(error => {
+        console.error(error);
+        setError("Erreur lors de la récupération de la météo.");
+      });
+  }, [urlapi]);
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!data) {
+    return <p>Chargement...</p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenair">
+      <h1 className="pays">{data.lieu.pays}</h1>
+      <h3 className="ville">• {data.lieu.nom}</h3>
+      <div className="img">
+        <img id="weatherImage" src={`https:${data.meteo.icone}`} alt="weather" />
+      </div>
+      <p className="temp">{`${data.actuel.temp_c} °C / ${data.actuel.temp_f} °F`}</p>
+      <p className="meteo">{data.meteo.principal}</p>
+      <div className="lat-lon">
+        <p className="lat">Latitude : {data.lat}°</p>
+        <p className="lon">Longitude : {data.lon}°</p>
+      </div>
     </div>
   );
 }
